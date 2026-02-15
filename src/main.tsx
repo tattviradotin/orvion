bc0a0295d2ebe1706ba00360f148edca2a4e4083
tattviradotin <tattvira@gmail.com>
2026-02-15 14:15:34 +0530

    final

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { CountdownGate } from './components/CountdownGate.tsx';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <CountdownGate>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-        </Routes>
-      </BrowserRouter>
-    </CountdownGate>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
